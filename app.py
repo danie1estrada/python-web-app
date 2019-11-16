34a74109597f41d74497ca86dc8bade8959daaaa
Daniel Estrada <isc.daniel.estrada@gmail.com>
2019-11-16 15:42:58 -0600

    ...

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, jsonify
-# from pyswip import Prolog
+from pyswip import Prolog
 
 app = Flask(__name__)
-# prolog = Prolog()
 
 @app.route('/')
 def main():
-    # p.consult('knowledge_base.pl')
+    prolog = Prolog()
+    p.consult('knowledge_base.pl')
     resp = { 'stops': [] }
-    # for i in p.query('route(\'A-71\', X)'):
-    #     resp['stops'].append(i['X'])
+    for i in p.query('route(\'A-71\', X)'):
+        resp['stops'].append(i['X'])
 
     return jsonify(resp)
 
