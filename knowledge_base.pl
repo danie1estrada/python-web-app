d626f3b9a977047dcb88a46df516dad153335bd1
Daniel Estrada <isc.daniel.estrada@gmail.com>
2019-11-16 15:53:15 -0600

    ...

--- knowledge_base.pl
+++ knowledge_base.pl
@@ -1,8 +1,8 @@
 % Facts
 % Routes: route, 
 route('A-71', 'Valle Dorado').
-route('A-71', 'Estación Delta').
+route('A-71', 'Estacion Delta').
 route('A-35', 'Valle Dorado').
-route('A-35', 'Estación Delta').
+route('A-35', 'Estacion Delta').
 
 from_to(Origin, Desntiny, Route):- route(Route, Origin), route(Route, Desntiny).
