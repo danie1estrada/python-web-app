09b84e813746a576fc8856f28a7e9e12e14eb236
Daniel Estrada <isc.daniel.estrada@gmail.com>
2019-11-19 17:21:10 -0600

    ...

--- app.py
+++ app.py
@@ -18,15 +18,42 @@ def get_routes_info():
     p = Prolog()
     p.consult('knowledge_base.pl')
     route = request.args.get('route')
-    resp = {
-        'ruta': route,
-        'paradas': []
+    response = {
+        'route': route,
+        'stops': []
     }
 
-    for paradero in p.query(f"route('{route}', X)"):
-        resp['paradas'].append(paradero['X'])
+    for solution in p.query(f"route('{route}', X)"):
+        resp['stops'].append(solution['X'])
 
-    return jsonify(resp)
+    return jsonify(response)
+
+@app.route('/consult/directions')
+def get_directions():
+    p = Prolog()
+    p.consult('knowledge_base.pl')
+    origin = request.args.get('origin')
+    destination = request.args.get('destination')
+    response = {
+        'availableRoutes': [
+
+        ]
+    }
+
+    # { route: '', next}
+    for solution in p.query(f"from_to('{origin}', '{destination}', Route)"):
+        pass
+
+    for solution in p.query(f"schedule('{}', '', S)"):
+        pass
+        
+    return jsonify(response)
+
+@app.route('/consult/schedules')
+def get_schedules():
+    p = Prolog()
+    p.consult('knowledge_base.pl')
+    route = request.args.get('route')
 
 if __name__ == '__main__':
     app.run()
